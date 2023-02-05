69942be1f2660fb7af25cb8adfbe6ecc677ce1a1
kubota@death.co.jp <kubota@death.co.jp>
2023-02-05 18:53:52 +0900

    zoomのAPIキーまで更新、いらないファイル除去

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from communications import comm
 import startVideoChat
 import channelObserver
 import webview
@@ -6,13 +5,3 @@ import adminScreen
 
 
 adminScreen.admin_screen()
-
-
-def eventKick(arg):
-    print('kick' + str(arg))
-
-
-if __name__ == '__main__':
-    # イベントキック検証
-    comm = comm.Comm('/dev/cu.usbmodem1411', 9)
-    comm += eventKick
--- sketch_channel_change_test/sketch_channel_change_test.ino
+++ sketch_channel_change_test/sketch_channel_change_test.ino
@@ -1,35 +1,58 @@
-int chan = 1;
-int val_2 = 0;
-int val_3 = 0;
-int val_4 = 0;
-int old_val_2 = 0;
-int old_val_3 = 0;
-int old_val_4 = 0;
-
-void setup(){
+// 11チャンネル
+int oldval = -1;
+void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
-  pinMode(4, INPUT_PULLUP);
+  for (int i = 2; i < 13; i++) {
+    pinMode(i, INPUT_PULLUP);
+  }
 }
-void loop(){
-   val_2 = digitalRead(2); // 入力を読み取りvalに格納
-   val_3 = digitalRead(3); // 入力を読み取りvalに格納
-   val_4 = digitalRead(4); // 入力を読み取りvalに格納
-  //変化があるかどうかチェック
-  if((val_2 == HIGH) && (old_val_2 == LOW)) {
-    Serial.println(1,DEC);
-    delay(100);
+
+void loop() {
+  delay(1000);
+  for (int i = 2; i < 13; i++) {
+    int val = digitalRead(i);
+    if (val == 0) {
+      if (oldval != i) {
+        oldval = i;
+        Serial.println(i-1, DEC);
+      }
+      break;
+    }
   }
-  if((val_3 == HIGH) && (old_val_3 == LOW)) {
-    Serial.println(2,DEC);
-    delay(100);
-  }
-  if((val_4 == HIGH) && (old_val_4 == LOW)) {
-    Serial.println(3,DEC);
-    delay(100);
-  }
-  old_val_2 = val_2;  // valはもう古くなったので保管しておく
-  old_val_3 = val_3;  // valはもう古くなったので保管しておく
-  old_val_4 = val_4;  // valはもう古くなったので保管しておく
 }
+
+// int chan = 1;
+// int val_2 = 0;
+// int val_3 = 0;
+// int val_4 = 0;
+// int old_val_2 = 0;
+// int old_val_3 = 0;
+// int old_val_4 = 0;
+
+// void setup(){
+//   Serial.begin(9600);
+//   pinMode(2, INPUT_PULLUP);
+//   pinMode(3, INPUT_PULLUP);
+//   pinMode(4, INPUT_PULLUP);
+// }
+// void loop(){
+//    val_2 = digitalRead(2); // 入力を読み取りvalに格納
+//    val_3 = digitalRead(3); // 入力を読み取りvalに格納
+//    val_4 = digitalRead(4); // 入力を読み取りvalに格納
+//   //変化があるかどうかチェック
+//   if((val_2 == HIGH) && (old_val_2 == LOW)) {
+//     Serial.println(1,DEC);
+//     delay(100);
+//   }
+//   if((val_3 == HIGH) && (old_val_3 == LOW)) {
+//     Serial.println(2,DEC);
+//     delay(100);
+//   }
+//   if((val_4 == HIGH) && (old_val_4 == LOW)) {
+//     Serial.println(3,DEC);
+//     delay(100);
+//   }
+//   old_val_2 = val_2;  // valはもう古くなったので保管しておく
+//   old_val_3 = val_3;  // valはもう古くなったので保管しておく
+//   old_val_4 = val_4;  // valはもう古くなったので保管しておく
+// }
